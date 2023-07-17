c4b04b05ec41d720a487fa510691f234c1addf05
1364656Murat <murat_1437@hotmail.com>
2023-07-17 18:02:50 +0300

    Fixed to bugs - repair operations

--- src/layouts/Dashboard.jsx
+++ src/layouts/Dashboard.jsx
@@ -6,13 +6,12 @@ import About from "../components/About";
 import HowCall from "./HowCall";
 import Contacts from "./Contacts";
 import Faq from "./Faq";
-import Main from "./Main";
 
 function Dashboard() {
     return (
         <div>
             <Navbar/>
-            <Route exact path={"/"} component ={Main}/>
+            {/*<Route exact path={"/"} component ={Main}/>*/}
             <Route exact path={"/about"} component={About}/>
             <Route exact path={"/how-call"} component={HowCall}/>
             <Route exact path={"/faq"} component={Faq}/>

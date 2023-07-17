328d7006d00c5c1b029a8029a0fdfb9be01aad0e
1364656Murat <murat_1437@hotmail.com>
2023-07-17 16:15:36 +0300

    Fixed to bugs - repair operations

--- src/layouts/Dashboard.jsx
+++ src/layouts/Dashboard.jsx
@@ -3,9 +3,6 @@ import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import {Route} from "react-router-dom";
 import About from "../components/About";
-import HowCall from "./HowCall";
-import Faq from "./Faq";
-import Contacts from "./Contacts";
 
 function Dashboard() {
     return (
@@ -13,9 +10,9 @@ function Dashboard() {
             <Navbar/>
             {/*<Route exact path={"/"} component ={Main}/>*/}
             <Route exact path={"/about"} component={About}/>
-            <Route exact path={"/how-call"} component={HowCall}/>
-            <Route exact path={"/faq"} component={Faq}/>
-            <Route exact path={"/contacts"} component={Contacts}/>
+            {/*<Route exact path={"/how-call"} component={HowCall}/>*/}
+            {/*<Route exact path={"/faq"} component={Faq}/>*/}
+            {/*<Route exact path={"/contacts"} component={Contacts}/>*/}
             <Footer/>
         </div>
     );

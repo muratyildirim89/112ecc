b3b1f56223a972241f14c9ce14420851a74f1ae3
1364656Murat <murat_1437@hotmail.com>
2023-07-17 15:23:51 +0300

    Fixed to bugs - 2

--- src/components/InfoArea.jsx
+++ src/components/InfoArea.jsx
@@ -1,103 +1,10 @@
-import React, {useState} from 'react';
+import React from 'react';
 import "../assets/css/Information.css"
-import {useTranslation} from "react-i18next";
-import {Link, useHistory} from "react-router-dom";
-import {classNames} from "../utilities/utils";
 
 function InfoArea() {
-    const history = useHistory();
 
-    const {t} = useTranslation();
-    const [activeTab, setActiveTab] = useState(history.location.pathname);
-
-    return (<div className={"area text-center"}>
-        {t("When should we call the emergency line")}?
-
-        <div className={"contents"}>
-            <div className="row">
-                <div className="col">
-                    <span className="material-symbols-rounded">local_police</span>
-                </div>
-                <div className="col col-colored">
-                    {t("Police")}
-                    <div className={"button-style"}>
-                        <Link to="/how-call" className={classNames({
-                            "nav-link": true,
-                            "active": activeTab === "/how-call",
-                            "link-dark": activeTab !== "/how-call"
-                        })}>
-                            {t("When To Call")}?
-                        </Link>
-                    </div>
-                </div>
-                <div className="col">
-                    <span className="material-symbols-rounded">ambulance</span>
-                </div>
-                <div className="col col-colored">{t("Ambulance")}
-                    <div className={"button-style"}>
-                        <Link to="/how-call" className={classNames({
-                            "nav-link": true,
-                            "active": activeTab === "/how-call",
-                            "link-dark": activeTab !== "/how-call"
-                        })}>
-                            {t("When To Call")}?
-                        </Link>
-                    </div>
-                </div>
-            </div>
-            <div className="row">
-                <div className="col col-colored">{t("Fire Department")}
-                    <div className={"button-style"}>
-                        <Link to="/how-call" className={classNames({
-                            "nav-link": true,
-                            "active": activeTab === "/how-call",
-                            "link-dark": activeTab !== "/how-call"
-                        })}>
-                            {t("When To Call")}?
-                        </Link>
-                    </div>
-                </div>
-                <div className="col">
-                    <span className="material-symbols-rounded">fire_truck</span>
-                </div>
-                <div className="col col-colored">{t("Forest Department")}
-                    <div className={"button-style"}>
-                        <Link to="/how-call" className={classNames({
-                            "nav-link": true,
-                            "active": activeTab === "/how-call",
-                            "link-dark": activeTab !== "/how-call"
-                        })}>
-                            {t("When To Call")}?
-                        </Link>
-                    </div>
-                </div>
-                <div className="col">
-                    <span className="material-symbols-rounded">forest</span>
-                </div>
-            </div>
-            <div className="row">
-                <div className="col">
-                    <span className="material-symbols-rounded">earthquake</span>
-                </div>
-                <div className="col col-colored">{t("Disaster")}
-                    <div className={"button-style"}>
-                        <Link to="/how-call" className={classNames({
-                            "nav-link": true,
-                            "active": activeTab === "/how-call",
-                            "link-dark": activeTab !== "/how-call"
-                        })}>
-                            {t("When To Call")}?
-                        </Link>
-                    </div>
-                </div>
-                <div className="col">
-                    <span className="material-symbols-rounded" style={{fontSize: "4vw"}}>emergency_share vaccines emergency</span>
-                </div>
-                <div className="col col-colored">{t("Coordination")}
-                    <div className={"button-style"}>{t("How To Use")}?</div>
-                </div>
-            </div>
-        </div>
+    return (<div>
+        InfoArea
     </div>);
 }
 

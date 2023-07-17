91cc0df4fb7ffb397004277b4a0bd58a97d3cc3d
1364656Murat <murat_1437@hotmail.com>
2023-07-17 11:39:21 +0300

    Fixed - files

---
+++ src/utilities/constants.js
@@ -0,0 +1,2 @@
+export const MONTH_NAMES = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+export const DAY_NAMES = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];

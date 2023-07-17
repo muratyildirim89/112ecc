c53d87e1e6e32c6482fcc649b79659f66f2b5f49
1364656Murat <murat_1437@hotmail.com>
2023-07-17 16:03:17 +0300

    Fixed to bugs - undo operations

--- src/layouts/Dashboard.jsx
+++ src/layouts/Dashboard.jsx
@@ -1,23 +1,16 @@
 import React from 'react';
-import Navbar from "../components/Navbar";
-import {Route} from "react-router-dom";
-import Main from "./Main";
-import Footer from "../components/Footer";
-import About from "../components/About";
-import HowCall from "./HowCall";
-import Faq from "./Faq";
-import Contacts from "./Contacts";
 
 function Dashboard() {
     return (
         <div>
-            <Navbar/>
-            <Route exact path={"/"} component ={Main}/>
-            <Route exact path={"/about"} component ={About}/>
-            <Route exact path={"/how-call"} component ={HowCall}/>
-            <Route exact path={"/faq"} component ={Faq}/>
-            <Route exact path={"/contacts"} component ={Contacts}/>
-            <Footer/>
+            Navbar ve tayfası :)
+            {/*<Navbar/>*/}
+            {/*<Route exact path={"/"} component ={Main}/>*/}
+            {/*<Route exact path={"/about"} component ={About}/>*/}
+            {/*<Route exact path={"/how-call"} component ={HowCall}/>*/}
+            {/*<Route exact path={"/faq"} component ={Faq}/>*/}
+            {/*<Route exact path={"/contacts"} component ={Contacts}/>*/}
+            {/*<Footer/>*/}
         </div>
     );
 }

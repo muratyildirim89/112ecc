4db1698573a3928603343aaa6d1f6076b58d0957
1364656Murat <murat_1437@hotmail.com>
2023-07-17 16:05:10 +0300

    Fixed to bugs - repair operations

--- src/layouts/Dashboard.jsx
+++ src/layouts/Dashboard.jsx
@@ -1,16 +1,17 @@
 import React from 'react';
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
 
 function Dashboard() {
     return (
         <div>
-            Navbar ve tayfası :)
-            {/*<Navbar/>*/}
+            <Navbar/>
             {/*<Route exact path={"/"} component ={Main}/>*/}
             {/*<Route exact path={"/about"} component ={About}/>*/}
             {/*<Route exact path={"/how-call"} component ={HowCall}/>*/}
             {/*<Route exact path={"/faq"} component ={Faq}/>*/}
             {/*<Route exact path={"/contacts"} component ={Contacts}/>*/}
-            {/*<Footer/>*/}
+            <Footer/>
         </div>
     );
 }

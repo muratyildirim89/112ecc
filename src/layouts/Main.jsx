fca2a2cc0c011a92b70012577e86aad9b37849b4
1364656Murat <murat_1437@hotmail.com>
2023-07-17 18:04:49 +0300

    Fixed to bugs - repair operations

--- src/layouts/Main.jsx
+++ src/layouts/Main.jsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import "../assets/css/Main.css"
 import MainShow from "../components/MainShow";
-import InfoArea from "../components/InfoArea";
 
 function Main() {
     return (
         <div>
             <MainShow/>
-            <InfoArea/>
+            {/*<InfoArea/>*/}
         </div>
     );
 }
